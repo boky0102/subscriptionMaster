fb1ca27e54f0122d96829e892357a28aa8428180
boky0102 <bivankovi@protonmail.com>
2023-08-04 22:57:25 +0200

    Added form handler function

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,57 @@
-
 import './App.css'
 import Header from './components/Header'
 import LoginForm from './components/LoginForm'
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  interface User{
+    username: string | undefined,
+    password: string | undefined ,
+    confirmPassword: string | undefined
+  }
+
+  const [formData, setFormData] = useState<User>({} as User);
   
+  function handleFormChange(event: React.ChangeEvent<HTMLInputElement>){
+    const {value, name} = event.currentTarget;
+    if(name === "username"){
+      setFormData((prevData) => (
+        {
+          ...prevData,
+          [ name ] : value
+        }
+      ))
+    } else if(name === "password"){
+      setFormData((prevData) => (
+        {
+          ...prevData,
+          [ name ] : value
+        }
+      ))
+    } else{
+      setFormData((prevData) => (
+        {
+          ...prevData,
+          [ name ] : value
+        }
+      ))
+    }
+    
+    
+  }
+
+  useEffect(() => {
+    console.log(formData);
+  }, [formData] );
+
+
+
 
   return (
     <>
       <Header></Header>
-      <LoginForm type="register"></LoginForm>
+      <LoginForm type="register" inputChange={handleFormChange}></LoginForm>
     </>
   )
 }
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,25 +1,32 @@
 import Button from "./Button"
+import { FC } from "react"
 
-function LoginForm(props: {type: "login" | "register"}){
-
+type loginFormProps = 
+    JSX.IntrinsicElements["input"] & {
+        type: "login" | "register",
+        inputChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+    
+}
+ 
+const LoginForm: FC<loginFormProps> = (props: loginFormProps) => {
 
     return(
             <div id="buttons-container">
                 <form>
                     <div>
                         <label>Username</label>
-                        <input type="text"></input>
+                        <input type="text" name="username" onChange={(props.inputChange)}></input>
                     </div>
                     <div>
                         <label>Password</label>
-                        <input type="password"></input>
+                        <input type="password" name="password" onChange={props.inputChange}></input>
                         {/* <Button label="Register" type="submit"></Button> */}
                     </div>
                     {
                         props.type === "register" &&
                         <div>
                             <label>{props.type}</label>
-                            <input type="password"></input>
+                            <input type="password" name="confirmPassword" onChange={props.inputChange}></input>
                         </div>
 
                     }
