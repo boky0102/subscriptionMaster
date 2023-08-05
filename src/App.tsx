ff8be4c383cf9c3706faa1c78f3d7481706947e9
boky0102 <bivankovi@protonmail.com>
2023-08-05 15:07:30 +0200

    Added handler for user form submit

--- src/App.tsx
+++ src/App.tsx
@@ -2,51 +2,9 @@ import './App.css'
 import Header from './components/Header'
 import Home from './components/Home';
 import LoginForm from './components/LoginForm'
-import { useState, useEffect } from 'react'
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-
-  interface User{
-    username: string | undefined,
-    password: string | undefined ,
-    confirmPassword: string | undefined
-  }
-
-  const [formData, setFormData] = useState<User>({} as User);
-  
-  function handleFormChange(event: React.ChangeEvent<HTMLInputElement>){
-    const {value, name} = event.currentTarget;
-    if(name === "username"){
-      setFormData((prevData) => (
-        {
-          ...prevData,
-          [ name ] : value
-        }
-      ))
-    } else if(name === "password"){
-      setFormData((prevData) => (
-        {
-          ...prevData,
-          [ name ] : value
-        }
-      ))
-    } else{
-      setFormData((prevData) => (
-        {
-          ...prevData,
-          [ name ] : value
-        }
-      ))
-    }
-    
-    
-  }
-
-  useEffect(() => {
-    console.log(formData);
-  }, [formData] );
-
 
 
 
@@ -55,8 +13,8 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/login" element={<LoginForm type="login" inputChange={handleFormChange}></LoginForm>}></Route>
-        <Route path="/register" element={<LoginForm type="register" inputChange={handleFormChange}></LoginForm>}></Route>
+        <Route path="/login" element={<LoginForm type="login"></LoginForm>}></Route>
+        <Route path="/register" element={<LoginForm type="register"></LoginForm>}></Route>
       </Routes>
     </>
   )
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,32 +1,95 @@
 import Button from "./Button"
-import { FC } from "react"
+import { useState, useEffect} from "react"
+import axios from "axios";
 
-type loginFormProps = 
-    JSX.IntrinsicElements["input"] & {
-        type: "login" | "register",
-        inputChange: (event: React.ChangeEvent<HTMLInputElement>) => void
-    
+type loginFormProps = {
+    type: "login" | "register"
 }
- 
-const LoginForm: FC<loginFormProps> = (props: loginFormProps) => {
+
+interface User {
+    username: string,
+    password: string,
+    confirmPassword: string | undefined
+}
+
+const LoginForm = (props: loginFormProps) => {
+
+    const [formData, setFormData] = useState({} as User);
+
+    function handleFormChange(event: React.ChangeEvent<HTMLInputElement>){
+        const { name, value } = event.currentTarget;
+        if(name === "username"){
+            setFormData((prevData) => (
+                {
+                    ...prevData,
+                    [ name ] : value
+                }
+            ))
+        } else if( name === "password" ){
+            setFormData((prevData) => (
+                {
+                    ...prevData,
+                    [ name ] : value
+                }
+            ))
+        } else {
+            setFormData((prevData) => (
+                {
+                    ...prevData,
+                    [ name ] : value
+                }
+            ))
+        }
+
+    }
+
+    function handleFormSubmit(event: React.FormEvent<HTMLFormElement>){
+        event.preventDefault();
+        let serverPath = import.meta.env.VITE_SERVER_LINK;
+        if(props.type === "login"){
+            serverPath += "/login";
+        } else {
+            serverPath += "/register";
+        }
+
+        axios.post(serverPath, formData, 
+            {
+                headers: {
+                    'Content-Type' : 'application/json'
+                }
+            })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+
+    }
+
+    useEffect(() => {
+        console.log(formData);
+    }, [formData]);
+    
+    
 
     return(
             <div id="buttons-container">
-                <form>
+                <form onSubmit={handleFormSubmit}>
                     <div>
                         <label>Username</label>
-                        <input type="text" name="username" onChange={(props.inputChange)}></input>
+                        <input type="text" name="username" onChange={handleFormChange}></input>
                     </div>
                     <div>
                         <label>Password</label>
-                        <input type="password" name="password" onChange={props.inputChange}></input>
+                        <input type="password" name="password" onChange={handleFormChange}></input>
                         {/* <Button label="Register" type="submit"></Button> */}
                     </div>
                     {
                         props.type === "register" &&
                         <div>
                             <label>{props.type}</label>
-                            <input type="password" name="confirmPassword" onChange={props.inputChange}></input>
+                            <input type="password" name="confirmPassword" onChange={handleFormChange}></input>
                         </div>
 
                     }
