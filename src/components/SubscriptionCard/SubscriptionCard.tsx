5327750bb8221d74e46f319c849646eda80a4c49
boky0102 <bivankovi@protonmail.com>
2023-10-18 10:32:19 +0200

    Restructured subscription card component and  added next renewal data

--- src/components/SubscriptionCard/SubscriptionCard.tsx
+++ src/components/SubscriptionCard/SubscriptionCard.tsx
@@ -1,7 +1,7 @@
 import { Subscription } from '../MySubscriptions/Mysubscriptions';
 import './SubscriptionCard.css';
 import Slider from '../Slider/Slider';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Notification } from '../Main/Home';
 
@@ -16,6 +16,16 @@ export default function SubscriptionCard(props: SubscriptionCardProps) {
      //wirte function for notification controll that will ift state up to hamecontent every time notification is trigerred
 
      const [sliderActive, setSliderActive] = useState(props.subscription.emailNotification);
+     const [nextRenewal, setNextRenewal] = useState(props.subscription.renewalDate);
+     const currentDate = new Date();
+
+     useEffect(() => {
+          if (props.subscription.renewalDate.getDate() <= currentDate.getDate()) {
+               const newRenewalDate = new Date(nextRenewal);
+               newRenewalDate.setMonth(currentDate.getMonth() + 1);
+               setNextRenewal(newRenewalDate);
+          }
+     }, [props.subscription]);
 
      function handleSliderChange() {
           if (sliderActive) {
@@ -52,11 +62,26 @@ export default function SubscriptionCard(props: SubscriptionCardProps) {
                     <div className="subscription-amount">{props.subscription.chargeAmount}</div>
                </div>
                <div className="division-line"></div>
-               <div className="subscription-card-element">Date started</div>
-               <div className="subscription-card-element">{props.subscription.dateAdded.toDateString()}</div>
-               <div className="subscription-card-element">Category</div>
-               <div className="subscription-card-element">{props.subscription.category}</div>
                <div className="subscription-card-element">
+                    <div>Date started</div>
+                    <div>{props.subscription.dateAdded.toDateString()}</div>
+               </div>
+               <div className="subscription-card-element">
+                    <div>Next renewal</div>
+                    <div>
+                         {nextRenewal.toLocaleDateString('en-GB', {
+                              month: 'long',
+                              day: 'numeric',
+                              year: 'numeric',
+                         })}
+                    </div>
+               </div>
+               <div className="subscription-card-element">
+                    <div>Category</div>
+                    <div>{props.subscription.category}</div>
+               </div>
+               <div className="subscription-card-element">
+                    <div>Email notification</div>
                     <Slider sliderActive={sliderActive} handleSliderClick={handleSliderChange}></Slider>
                </div>
                <div className="subscription-card-element">
