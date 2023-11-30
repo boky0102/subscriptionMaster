c6bcfbd0516bf805d8be6266733911fd722f20a6
boky0102 <bivankovi@protonmail.com>
2023-11-30 13:47:40 +0100

    Added styling on navigation panel for current page

--- src/components/Dashboard/DashboardButton.tsx
+++ src/components/Dashboard/DashboardButton.tsx
@@ -1,10 +1,11 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './DashboardButton.css';
 import SettingsIcon from '../Icons/SettingsIconSvg';
 import CalendarIcon from '../Icons/CalendarIconSvg';
 import AddIcon from '../Icons/AddIconSvg';
 import HistoryIcon from '../Icons/HistoryIconSvg';
 import HomeIconSvg from '../Icons/HomeIconSvg';
+import { useState } from 'react';
 
 interface props {
      label: string;
@@ -19,13 +20,47 @@ interface props {
 }
 
 function DashboardButton(props: props) {
+     const currentLocation = useLocation().pathname.split('/')[2];
+
      return (
           <Link to={`/home/${props.route}`} className="dashboard-link dashboard-button-container">
-               {props.route === 'settings' && <SettingsIcon className="settings-icon"></SettingsIcon>}
-               {props.route === 'callendar' && <CalendarIcon className="settings-icon"></CalendarIcon>}
-               {props.route === 'addsubscription' && <AddIcon className="settings-icon"></AddIcon>}
-               {props.route === 'mysubscriptions' && <HistoryIcon className="settings-icon"></HistoryIcon>}
-               {props.route === '' && <HomeIconSvg className="settings-icon"></HomeIconSvg>}
+               {props.route === 'settings' && (
+                    <SettingsIcon
+                         className={
+                              currentLocation === 'settings' ? 'settings-icon settings-icon-active' : 'settings-icon'
+                         }
+                    ></SettingsIcon>
+               )}
+               {props.route === 'callendar' && (
+                    <CalendarIcon
+                         className={
+                              currentLocation === 'callendar' ? 'settings-icon settings-icon-active' : 'settings-icon'
+                         }
+                    ></CalendarIcon>
+               )}
+               {props.route === 'addsubscription' && (
+                    <AddIcon
+                         className={
+                              currentLocation === 'addsubscription'
+                                   ? 'settings-icon settings-icon-active'
+                                   : 'settings-icon'
+                         }
+                    ></AddIcon>
+               )}
+               {props.route === 'mysubscriptions' && (
+                    <HistoryIcon
+                         className={
+                              currentLocation === 'mysubscriptions'
+                                   ? 'settings-icon settings-icon-active'
+                                   : 'settings-icon'
+                         }
+                    ></HistoryIcon>
+               )}
+               {props.route === '' && (
+                    <HomeIconSvg
+                         className={currentLocation === '' ? 'settings-icon settings-icon-active' : 'settings-icon'}
+                    ></HomeIconSvg>
+               )}
                <div className="link-container">
                     <div className="link-label">{props.label}</div>
                     <div className="link-underscore"></div>
